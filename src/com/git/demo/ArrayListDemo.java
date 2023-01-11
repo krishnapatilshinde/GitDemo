665da97aab798fa439deb03093e8141c1f5f14d8
krishna <krishnapatilshinde96@gmail.com>
2023-01-11 21:54:27 +0530

    First commit is : added ArrayList Demo Code

---
+++ src/com/git/demo/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.git.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		
+		ArrayList<String> arraylist=new ArrayList<String>();
+		arraylist.add("Krishna");
+		arraylist.add("pooja");
+		arraylist.add("gopal");
+		
+		System.out.println(arraylist);
+	}
+
+}
