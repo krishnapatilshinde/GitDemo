a2eea96b6314e9eb7538a8abb2e5a86c6a281d48
krishna <krishnapatilshinde96@gmail.com>
2023-01-11 22:16:54 +0530

    second commit : added linked list code

---
+++ src/com/git/demo/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package com.git.demo;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+public static void main(String[] args) {
+	
+	
+	LinkedList<Integer> linkedlist=new LinkedList<Integer>();
+	linkedlist.add(1);
+	linkedlist.add(2);
+	linkedlist.add(3);
+	linkedlist.add(4);
+	linkedlist.add(5);
+	
+	System.out.println("The Elements of An LinkedList is-->>"+linkedlist);
+}
+}
