17aada4912b36cb2509d349269fd5069224b3118
Krishna <krishnapatil96@gmail.com>
2023-01-12 23:48:04 +0530

    added hashmap code

---
+++ src/main/java/com/spring/HashMapDemo.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+	
+	public static void main(String[] args) {
+		HashMap hashmap=new HashMap();
+		
+		hashmap.put(1, "krishna");
+		hashmap.put(2, "shinde");
+		
+		
+		System.out.println(hashmap);
+		
+	}
+
+}
