d072bc1e2c2fc3390ac409ea3b1122caf0dee77e
Krishna <krishnapatil96@gmail.com>
2023-01-13 00:05:59 +0530

    first commit : added sample class

---
+++ src/main/java/com/spring/NewSampleClass.java
@@ -0,0 +1,10 @@
+package com.spring;
+
+public class NewSampleClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is sample class created only for git understanding");
+	}
+
+}
